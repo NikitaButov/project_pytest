d25743c6a6b2948ed98f09dccbad5cc95f48db45
Nikita Butov <butov3084@gmail.com>
2023-07-30 11:28:01 +0500

    Исправил тесты к модулю arrs.py test_my_slice

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,5 @@
 from utils import arrs
+from utils.arrs import my_slice
 
 
 def test_get():
@@ -9,3 +10,9 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+def test_my_slice():
+    assert my_slice([1, 2, 3, 4, 5], 1, 4) == [2, 3, 4]
+    assert my_slice([1, 2, 3, 4, 5], -3, None) == [3, 4, 5]
+    assert my_slice([], 0, 10) == []
+    assert my_slice([1, 2, 3, 4, 5], -10, 3) == [1, 2, 3]
